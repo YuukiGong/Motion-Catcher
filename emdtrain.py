bd30fc623ee665f0301c15eca1087f30c84fd7e3
YuukiG <57143546+YuukiGong@users.noreply.github.com>
2025-04-01 16:27:39 +0800

    Update emdtrain.py

--- emdtrain.py
+++ emdtrain.py
@@ -91,7 +91,7 @@ def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, h
     current_sample = None
     for filesample in data:
         assert isinstance(filesample, dict)
-        fname, value = filesample["fname"], filesample["data"]
+        fname, value = filesample["fname"], filesample["data"] 
         prefix, suffix = keys(fname)
         if prefix is None:
             continue
